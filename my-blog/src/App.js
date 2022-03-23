531ac6eb714e97aebfac6762fd282d0f3d282f0a
Bahaulddin Shammary <bahaa.shammary@gmail.com>
2022-03-23 23:34:42 +0000

    Created a few components and added them as routes in app.js

--- my-blog/src/App.js
+++ my-blog/src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom"
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ArticlePage from './pages/ArticlePage';
+import ArticlesList from './pages/ArticlesList';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <div id='page-body'>
+          <Routes>
+            <Route path='/' element={<HomePage />} />
+            <Route path='/about' element={<AboutPage />} />
+            <Route path='/articles-list' element={<ArticlesList />} />
+            <Route path='article' element={<ArticlePage />} />
+          </Routes>
+
+        </div>
+      </div>
+    </Router>
   );
 }
 

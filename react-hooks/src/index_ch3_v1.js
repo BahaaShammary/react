59ccb1579d58fd325a7ad5d4d09a3d097cb95c61
Bahaulddin Shammary <bahaa.shammary@gmail.com>
2022-10-02 23:45:31 +0100

    More useEffect

--- react-hooks/src/index_ch3_v1.js
+++ react-hooks/src/index_ch3_v1.js
@@ -1,11 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// Chapter 3 - V1: Working with useEffect
+// Chapter 3 - V1 & V2: Working with useEffect
 const App = () => {
+    const [name, setName] = useState("Bahaa");
+    const [admin, setAdmin] = useState(false);
+
+    useEffect(() => {
+            document.title = `Celebrate ${name}`
+    },[name]);
+
+    useEffect(() => {
+        console.log(`The user is: ${admin ? "admin" : "not admin"}`);}, [admin]);
+
     return (
         <section>
-            <p>Congratulations!</p>
+            <p>Congratulations! {name}</p>
+            <button onClick={() => setName("Katrina")}>Change Winner</button>
+            <p>{admin ? "Logged in" : "Not logged in"}</p>
+            <button onClick={() => setAdmin(true)}>Log in</button>
         </section>
     );
 }
